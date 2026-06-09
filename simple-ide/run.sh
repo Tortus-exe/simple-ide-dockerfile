8d089b4162ff7c309e863b9fe00cca834ab563d7
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2026-06-09 00:36:46 -0700

    fix run files

--- simple-ide/run.sh
+++ simple-ide/run.sh
@@ -1,8 +1,16 @@
-docker run -it --rm \
-    -e DISPLAY=$DISPLAY \
-    -e XAUTHORITY=/tmp/.Xauthority \
-    -v $XAUTHORITY:/tmp/.Xauthority:ro \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-    --device /dev/dri \
-    --net=host \
-    simpleide
+#!/bin/sh
+
+contents=$(realpath $1)
+if [ -e $contents ]; then
+    docker run -it --rm \
+        -e DISPLAY=$DISPLAY \
+        -e XAUTHORITY=/tmp/.Xauthority \
+        -v $XAUTHORITY:/tmp/.Xauthority:ro \
+        -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+        -v $1:/home/app/contents \
+        --device /dev/dri \
+        --net=host \
+        simpleide
+else
+    echo "$contents DOES NOT EXIST!"
+fi
